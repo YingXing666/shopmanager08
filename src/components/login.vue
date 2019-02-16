afd604ca961da251f11a9bf66e88ed7531944006
YingXing666 <1078187773@qq.com>
2019-02-16 10:03:14 +0800

    first commit

---
+++ src/components/login.vue
@@ -0,0 +1,28 @@
+<template>
+  <el-form label-position="top" label-width="80px" :model="formdata">
+    <el-form-item label="用户名">
+      <el-input v-model="formdata.name"></el-input>
+    </el-form-item>
+    <el-form-item label="密码">
+      <el-input v-model="formdata.password"></el-input>
+    </el-form-item>
+      <el-button type="success">登录</el-button>
+  </el-form>
+  
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      formdata:{
+        name:'',
+        password:''
+      }
+    }
+  }
+};
+</script>
+
+<style>
+</style>
